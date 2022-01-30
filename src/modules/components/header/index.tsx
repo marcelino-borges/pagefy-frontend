3a644456257b101e987c94de131db6a6dd7b878e
Marcelino Borges <botelho_gt@hotmail.com>
2022-01-30 19:10:53 -0300

    header mobile

--- src/modules/components/header/index.tsx
+++ src/modules/components/header/index.tsx
@@ -1,40 +1,130 @@
-import { Grid } from "@mui/material";
-import { HeaderLink, Parent } from "./style";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import {
+  HeaderLinkDesktop,
+  Parent,
+  HamburguerMenuIcon,
+  DrawerMenu,
+  MobileMenuGridItem,
+} from "./style";
 import strings from "../../../localization";
 import routes from "./../../../routes/paths";
 import images from "../../../assets/img";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const theme = useTheme();
+  const isSmallerThanMD = useMediaQuery(theme.breakpoints.down("md"));
+  const navigate = useNavigate();
+
+  const [isShowingDrawer, setIsShowingDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsShowingDrawer(!isShowingDrawer);
+  };
+
+  useEffect(() => {
+    if (!isSmallerThanMD) {
+      setIsShowingDrawer(false);
+    }
+  }, [isSmallerThanMD]);
+
+  const onClickLinkCallback = (navigateTo: string) => {
+    setIsShowingDrawer(false);
+    navigate(navigateTo);
+  };
+
+  const DesktopHeader = () => {
+    return (
+      <>
+        <Grid container item xs={6} justifyContent="center">
+          <Grid item>
+            <HeaderLinkDesktop to={routes.root}>
+              {strings.home}
+            </HeaderLinkDesktop>
+          </Grid>
+          <Grid item>
+            <HeaderLinkDesktop to={routes.pages}>
+              {strings.pages}
+            </HeaderLinkDesktop>
+          </Grid>
+          <Grid item>
+            <HeaderLinkDesktop to={routes.support}>
+              {strings.support}
+            </HeaderLinkDesktop>
+          </Grid>
+        </Grid>
+
+        <Grid container item xs={3} justifyContent="center" padding={3}>
+          User Logged In
+        </Grid>
+      </>
+    );
+  };
+
+  const MobileDesktop = () => {
+    return (
+      <>
+        <Grid container item xs={6} justifyContent="flex-end" padding={4}>
+          <HamburguerMenuIcon onClick={toggleDrawer} />
+        </Grid>
+
+        <DrawerMenu
+          anchor="right"
+          open={isShowingDrawer}
+          onClose={toggleDrawer}
+        >
+          <Grid container direction="column" alignItems="center">
+            <Grid item>User Logged In</Grid>
+
+            <MobileMenuGridItem
+              item
+              alignItems="center"
+              onClick={() => onClickLinkCallback(routes.root)}
+            >
+              {strings.home}
+            </MobileMenuGridItem>
+
+            <MobileMenuGridItem
+              item
+              alignItems="center"
+              onClick={() => onClickLinkCallback(routes.pages)}
+            >
+              {strings.pages}
+            </MobileMenuGridItem>
+
+            <MobileMenuGridItem
+              item
+              alignItems="center"
+              onClick={() => onClickLinkCallback(routes.support)}
+            >
+              {strings.support}
+            </MobileMenuGridItem>
+          </Grid>
+        </DrawerMenu>
+      </>
+    );
+  };
+
   return (
     <Parent container alignItems="center">
       <Grid
         container
         item
-        xs={3}
-        justifyContent="center"
-        padding={3}
-        style={{ height: "100%" }}
+        xs={6}
+        md={3}
+        justifyContent="flex-start"
+        padding={4}
       >
-        <img
-          src={images.logoHeader}
-          style={{ height: "100%", width: "auto" }}
-          alt="Logo"
-        />
+        <Link to={routes.root}>
+          <img
+            src={images.logoHeader}
+            style={{ height: "61px", width: "auto" }}
+            alt="Logo"
+          />
+        </Link>
       </Grid>
-      <Grid container item xs={6} justifyContent="center">
-        <Grid item>
-          <HeaderLink to={routes.root}>{strings.home}</HeaderLink>
-        </Grid>
-        <Grid item>
-          <HeaderLink to={routes.pages}>{strings.pages}</HeaderLink>
-        </Grid>
-        <Grid item>
-          <HeaderLink to={routes.support}>{strings.support}</HeaderLink>
-        </Grid>
-      </Grid>
-      <Grid container item xs={3} justifyContent="center" padding={3}>
-        User Logged In
-      </Grid>
+      {isSmallerThanMD ? <MobileDesktop /> : <DesktopHeader />}
     </Parent>
   );
 };
--- src/modules/components/header/style.tsx
+++ src/modules/components/header/style.tsx
@@ -1,21 +1,22 @@
 import { styled } from "@mui/system";
-import { Grid } from "@mui/material";
+import { Grid, Drawer } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Menu } from "@mui/icons-material";
 
 export const Parent = styled(Grid)`
   border: 1px solid #e9e9e9;
   height: 125px;
   background-color: white;
+  min-width: 320px;
 `;
 
-export const HeaderLink = styled(Link)`
+export const HeaderLinkDesktop = styled(Link)`
   color: black;
-  padding-bottom: 51px; /* distance from text to underline when hover */
+  padding-bottom: 50px; /* distance from text to underline when hover */
   transition: all 0.2s ease-in-out;
   position: relative;
   height: 100%;
   margin-right: 24px;
-  font-weight: 300;
 
   &:hover {
     color: var(--primary);
@@ -38,5 +39,46 @@ export const HeaderLink = styled(Link)`
   &:hover:before {
     visibility: visible;
     transform: scaleX(1);
+  }
+`;
+
+export const HamburguerMenuIcon = styled(Menu)`
+  color: var(--primary);
+  font-size: 40px;
+  cursor: pointer;
+`;
+
+export const DrawerMenu = styled(Drawer)`
+  color: var(--primary);
+  padding: 32px 16px 32px 16px;
+
+  .MuiPaper-root.MuiDrawer-paper {
+    width: 50%;
+  }
+
+  @media (max-width: 500px) {
+    .MuiPaper-root.MuiDrawer-paper {
+      width: 75%;
+    }
+  }
+
+  @media (max-width: 400px) {
+    .MuiPaper-root.MuiDrawer-paper {
+      width: 90%;
+    }
+  }
+`;
+
+export const MobileMenuGridItem = styled(Grid)`
+  text-align: center;
+  transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+  border-left: 3px solid rgba(0, 0, 0, 0);
+  height: 64px;
+  padding-top: 18px;
+  cursor: pointer;
+
+  &:hover {
+    border-left: 3px solid var(--primary);
+    background-color: rgba(0, 0, 0, 0.1);
   }
 `;
