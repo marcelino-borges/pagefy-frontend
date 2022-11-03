c51613a70b82722a0ed2a7e994151f26344b056d
Marcelino Borges <botelho_gt@hotmail.com>
2022-11-03 01:30:47 +0000

    Added testimonial video preview to testimonial card in the profile

--- src/modules/pages/profile/testimonials/card/index.tsx
+++ src/modules/pages/profile/testimonials/card/index.tsx
@@ -3,6 +3,9 @@ import { ITestimonial } from "../../../../../store/testimonials/types";
 import RatingStars from "../rating-stars";
 import { BorderDiv, Root } from "./style";
 import moment from "moment";
+import YoutubeEmbed from "../../../../components/youtube-embed";
+import { getYoutubeIdFromUrl } from "../../../../../utils";
+import { useEffect, useState } from "react";
 
 interface IUserTestimonialCardProps {
   testimonial: ITestimonial;
@@ -13,6 +16,14 @@ const UserTestimonialCard = ({
   testimonial,
   onClick,
 }: IUserTestimonialCardProps) => {
+  const [videoId, setVideoId] = useState<string | null>("");
+
+  useEffect(() => {
+    if (testimonial.videoUrl?.length) {
+      setVideoId(getYoutubeIdFromUrl(testimonial.videoUrl));
+    }
+  }, [testimonial.videoUrl]);
+
   return (
     <Root container item direction="column" onClick={onClick}>
       <BorderDiv className="borderDiv" />
@@ -24,6 +35,15 @@ const UserTestimonialCard = ({
         />
         <i>{moment(testimonial.updatedAt).format("DD/MM/YYYY")}</i>
       </Stack>
+      {videoId && testimonial.videoUrl?.length && (
+        <Grid container pt="24px">
+          <YoutubeEmbed
+            width={`${500}px`}
+            height={`${(500 * 9) / 16}px`}
+            embedId={videoId}
+          />
+        </Grid>
+      )}
       <Grid container pt="24px">
         {testimonial.testimonial}
       </Grid>
