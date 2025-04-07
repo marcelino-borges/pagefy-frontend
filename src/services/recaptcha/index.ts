41efb5d16662a50add14c6b95988a9fcb04005a7
Marcelino Borges <>
2025-04-06 21:01:27 -0300

    fix: recaptcha verify api route changed

--- src/services/recaptcha/index.ts
+++ src/services/recaptcha/index.ts
@@ -4,6 +4,6 @@ import { registrationApi } from "../../config/axios";
 export const verifyRecaptcha = async (
   token: string
 ): Promise<AxiosResponse<any>> =>
-  await registrationApi.post(`/verify-recaptcha`, {
+  await registrationApi.post(`/recaptcha/verify`, {
     token,
   });
